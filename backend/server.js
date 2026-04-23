83769ea76e6224ae70e8b067c84a217ceaf30027
Cameron Geiser <cameron.geiser@gmail.com>
2026-04-23 16:52:49 -0700

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/database.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is up...");
+});
+
+app.listen(3000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:3000 s");
+})
+
