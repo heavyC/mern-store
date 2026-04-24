f0e939c17a3573a68c2ed570f60fa35bb277ce14
Cameron Geiser <cameron.geiser@gmail.com>
2026-04-24 14:08:50 -0700

    db updates

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,44 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database.js';
+import Product from '../models/product.model.js'
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is up...");
+app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find();
+        return res.status(400).json({ success: true, data: products }); // message: "Products retrieved successfully" });
+    }
+    catch (error) {
+        console.log("Error in get Products: ", error.message)
+        return res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
+    if (!product.name || !product.image || !product.price) {
+        return res.status(400).json({ success: false, message: "Please provide all fields." })
+    }
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, message: "Product created" });
+    }
+    catch (error) {
+        console.log("Error in create Product: ", error.message);
+        res.status(500).json({ success: false, message: "Server error" })
+    }
+});
+
+app.get("/products", (req, res) => {
+    res.send("products is up...");
 });
 
 app.listen(3000, () => {
